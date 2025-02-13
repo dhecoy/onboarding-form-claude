398251e8de1192183ac5207ad7230f594a936940
dhecoy <dennis@tadait.se>
2025-02-13 15:50:34 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,11 @@
+'use client'
+
+import OnboardingForm from '@/components/forms/OnboardingForm'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <OnboardingForm />
+    </main>
+  )
+}
